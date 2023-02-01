7322eea4d1a1a2267de8e57d8ed504f927e5e71a
Yusuf Burak Cakmak <57682287+ysfckmk@users.noreply.github.com>
2023-02-01 22:59:01 +0300

    Create chatGPT.py

---
+++ chatGPT.py
@@ -0,0 +1,38 @@
+import os
+import openai
+import pyttsx3
+import speech_recognition as sr
+
+
+def speechToText():
+    # obtain audio from the microphone
+  r = sr.Recognizer()
+  with sr.Microphone() as source:
+      print("Say something!")
+      audio = r.listen(source)
+
+
+
+  return(str(r.recognize_google(audio)))
+
+
+
+
+
+def askToGpt(ask):
+  openai.api_key = your-api-key
+  response = openai.Completion.create(
+    model='text-davinci-003',
+    prompt=ask,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+  rspText = response['choices'][0]['text']
+  engine = pyttsx3.init()
+  engine.setProperty('rate', 120)    
+  engine.say(rspText)
+  engine.runAndWait()
+  return rspText
